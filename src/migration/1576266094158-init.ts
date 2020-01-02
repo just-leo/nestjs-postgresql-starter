644d2d9092a08503a6c2d576718ca0367b942cfb
Leonid Ilin <l@codemotion.eu>
2020-01-02 17:39:39 +0200

    Project structure, scripts, configs

---
+++ src/migration/1576266094158-init.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class init1576266094158 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<any> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {
+    }
+
+}
