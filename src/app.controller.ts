e5df68cd38f667d5a468c1d998377842582472b0
Leonid Ilin <l@codemotion.eu>
2020-01-07 20:16:16 +0200

    Cron tasks using nest-schedule

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,16 +1,26 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Logger } from '@nestjs/common';
 import { ApiTags, ApiResponse, ApiOperation } from '@nestjs/swagger';
+import { Cron, Interval, Timeout, NestSchedule } from 'nest-schedule';
+import { AppService } from './app.service';
 
 @ApiTags('healthcheck')
 @Controller()
-export class AppController {
-  constructor(private readonly appService: AppService) {}
+export class AppController extends NestSchedule {
+  private readonly logger = new Logger(AppController.name);
+
+  constructor(private readonly appService: AppService) {
+    super();
+  }
 
   @Get()
   @ApiOperation({ summary: 'Health check route' })
   @ApiResponse({ status: 200, description: 'Ok' })
   healthCheck(): string {
     return this.appService.getStatus();
+  }
+
+  @Cron('*/5 * * * *', { immediate: false })
+  cronJob() {
+    this.logger.log('Cron job started');
   }
 }
